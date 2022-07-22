42f5fe942edd170d30e8cad1bd0eb0b04d47023e
yeligoth <yeligoth@gmail.com>
2022-07-22 16:56:38 -0500

    Final changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'primary': {
+        500: '#eb1152',
+        600: '#c70d46',
+      }
+    }
   },
   plugins: [],
 }
